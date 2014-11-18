05c18b5ff5d5ba437f32453782f7f6bfb510938f
Erik <info@zen4all.nl>
2014-11-18 13:50:56 +0100

    Version change in header

--- includes/languages/dutch/unsubscribe.php
+++ includes/languages/dutch/unsubscribe.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * @package languageDefines
+ * @package languageDefines Dutch Zen Cart Version 1.5.3
+ * @ Maintained by Zen4All (http://zen4all.nl)
  * @copyright Copyright 2003-2006 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @copyright Originally Programmed By: Christopher Bradley (www.wizardsandwars.com) for OsCommerce
  * @copyright Modified by Jim Keebaugh for OsCommerce
  * @copyright Adapted for Zen Cart
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: dutch translation voor zen cart 1.5.2
  * @version $Id: unsubscribe.php 3159 2006-03-11 01:35:04Z drbyte $
  */
 
