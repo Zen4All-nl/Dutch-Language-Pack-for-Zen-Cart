2e89957470feeb1510e4d66416176e1724cc6cd0
Zen4All <info@zen4all.nl>
2019-03-06 10:06:38 +0100

    Added a version installer
    Added a version installer to the module, so the current version so the language pack can be tracked.

---
+++ 1 - Installation files/YOUR_ADMIN/includes/auto_loaders/config.dutch_language_pack.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Dutch Language Pack
+ * @version 1.5.6b
+ * @author Zen4All
+ * @copyright (c) 2014-2019, Zen4All
+ * @license http://www.gnu.org/licenses/gpl.txt GNU General Public License V2.0
+ */
+$autoLoadConfig[999][] = array(
+  'autoType' => 'init_script',
+  'loadFile' => 'init_dutch_language_pack.php');
